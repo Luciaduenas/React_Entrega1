2ffcc4756e4e9eff27ac2e0b2a611b83772a55de
Lucia Duenas <marialuciaduenas@gmail.com>
2023-11-05 23:53:57 -0300

    commit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -9,7 +9,7 @@ import { FaPinterestSquare } from "react-icons/fa";
 export const Footer = () => {
     return ( 
         <FooterContainer>
-            <FooterLogo src="src/assets/images/LOGO HAPPY COLORS.png" alt="Happy Colors Logo" />
+            <FooterLogo src=".../src/assets/images/LOGO HAPPY COLORS.png" alt="Happy Colors Logo" />
             <SocialContainer>
                 <h4>Follow us on social Media</h4>
                 <SocialLinksContainer>
